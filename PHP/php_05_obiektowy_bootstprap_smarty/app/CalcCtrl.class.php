a118a249c37a4dec124d31652b47e269e99ccf6d
krzysztof-tannenberg <80205057+krzysztof-tannenberg@users.noreply.github.com>
2021-04-12 22:56:21 +0200

    Add files via upload

---
+++ PHP/php_05_obiektowy_bootstprap_smarty/app/CalcCtrl.class.php
@@ -0,0 +1,122 @@
+<?php
+
+require_once $conf->root_path.'/lib/smarty/Smarty.class.php';
+require_once $conf->root_path.'/lib/Messages.class.php';
+require_once $conf->root_path.'/app/CalcForm.class.php';
+require_once $conf->root_path.'/app/CalcResult.class.php';
+
+
+class CalcCtrl {
+
+	private $msgs;   //wiadomości dla widoku
+	private $form;   //dane formularza (do obliczeń i dla widoku)
+	private $result; //inne dane dla widoku
+
+
+	public function __construct(){
+		//stworzenie potrzebnych obiektów
+		$this->msgs = new Messages();
+		$this->form = new CalcForm();
+		$this->result = new CalcResult();
+	}
+	
+	/* 
+	  Pobranie parametrów
+	 */
+	public function getParams(){
+		$this->form->kwota = isset($_REQUEST ['kwota']) ? $_REQUEST ['kwota'] : null;
+		$this->form->czas = isset($_REQUEST ['czas']) ? $_REQUEST ['czas'] : null;
+		$this->form->oprocentowanie = isset($_REQUEST ['oprocentowanie']) ? $_REQUEST ['oprocentowanie'] : null;
+	}
+	
+	/* 
+	  Walidacja parametrów
+	  @return true jeśli brak błedów, false w przeciwnym wypadku 
+	 */
+	public function validate() {
+		// sprawdzenie, czy parametry zostały przekazane
+		if (! (isset ( $this->form->kwota ) && isset ( $this->form->czas ) && isset ( $this->form->oprocentowanie ))) {
+			// sytuacja wystąpi kiedy np. kontroler zostanie wywołany bezpośrednio - nie z formularza
+			return false; //zakończ walidację z błędem
+		}
+		
+		
+		if ($this->form->kwota == "") {
+			$this->msgs->addError('Nie podano kwoty');
+		}
+		if ($this->form->czas == "") {
+			$this->msgs->addError('Nie podano czasu');
+		}
+		if ($this->form->oprocentowanie == "") {
+			$this->msgs->addError('Nie podano oprocentowania');
+		}
+		
+		
+		if (! $this->msgs->isError()) {
+			
+			
+			if (! is_numeric ( $this->form->kwota )) {
+				$this->msgs->addError('Pierwsza wartość nie jest liczbą całkowitą');
+			}
+			
+			if (! is_numeric ( $this->form->czas )) {
+				$this->msgs->addError('Druga wartość nie jest liczbą całkowitą');
+			}
+			
+			if (! is_numeric ( $this->form->oprocentowanie )) {
+				$this->msgs->addError('Druga wartość nie jest liczbą całkowitą');
+			}
+		}
+		
+		return ! $this->msgs->isError();
+	}
+	
+	/* 
+	  Pobranie wartości, walidacja, obliczenie i wyświetlenie
+	 */
+	public function process(){
+
+		$this->getparams();
+		
+		if ($this->validate()) {
+				
+			//konwersja parametrów na int
+			$this->form->kwota = intval($this->form->kwota);
+			$this->form->czas = intval($this->form->czas);
+			$this->form->oprocentowanie = intval($this->form->oprocentowanie);
+			$this->msgs->addInfo('Parametry poprawne.');
+				
+			//wykonanie operacji
+		if ($this->form->kwota < 1 ){
+		$this->msgs->addError('Nie możesz liczyć dla kwot mniejszych od 1pln !');
+				
+			} else {
+				$this->result->result = $this->form->kwota / (12*$this->form->czas)+($this->form->kwota/(12*$this->form->czas))*$this->form->oprocentowanie;
+			}		
+			$this->msgs->addInfo('Wykonano obliczenia.');
+		}
+		
+		$this->generateView();
+	}
+	
+	
+	/*
+	  Widok
+	 */
+	public function generateView(){
+		global $conf;
+		
+		$smarty = new Smarty();
+		$smarty->assign('conf',$conf);
+		
+		$smarty->assign('page_title','Przykład 05');
+		$smarty->assign('page_description','Obiektowość przykład v1');
+		$smarty->assign('page_header','Obiekty w PHP');
+				
+		$smarty->assign('msgs',$this->msgs);
+		$smarty->assign('form',$this->form);
+		$smarty->assign('res',$this->result);
+		
+		$smarty->display($conf->root_path.'/app/CalcView.html');
+	}
+}
