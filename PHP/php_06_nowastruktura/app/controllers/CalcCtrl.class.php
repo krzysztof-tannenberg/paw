8de154fff26009edbb483268529e1168b6a60d78
krzysztof-tannenberg <80205057+krzysztof-tannenberg@users.noreply.github.com>
2021-04-26 22:42:14 +0200

    Add files via upload

---
+++ PHP/php_06_nowastruktura/app/controllers/CalcCtrl.class.php
@@ -0,0 +1,122 @@
+<?php
+// W skrypcie definicji kontrolera nie trzeba dołączać żadnego skryptu inicjalizacji.
+// Konfiguracja, Messages i Smarty są dostępne za pomocą odpowiednich funkcji.
+// Kontroler ładuje tylko to z czego sam korzysta.
+
+require_once 'CalcForm.class.php';
+require_once 'CalcResult.class.php';
+
+/** Kontroler kalkulatora
+ * @author Przemysław Kudłacik
+ *
+ */
+class CalcCtrl {
+
+	private $form;   //dane formularza (do obliczeń i dla widoku)
+	private $result; //inne dane dla widoku
+
+	/** 
+	 * Konstruktor - inicjalizacja właściwości
+	 */
+	public function __construct(){
+		//stworzenie potrzebnych obiektów
+		$this->form = new CalcForm();
+		$this->result = new CalcResult();
+	}
+	
+	/** 
+	 * Pobranie parametrów
+	 */
+	public function getParams(){
+		$this->form->kwota = getFromRequest('kwota');
+		$this->form->czas = getFromRequest('czas');
+		$this->form->oprocentowanie = getFromRequest('oprocentowanie');
+	}
+
+	/* 
+	  Walidacja parametrów
+	  @return true jeśli brak błedów, false w przeciwnym wypadku 
+	 */
+	public function validate() {
+		// sprawdzenie, czy parametry zostały przekazane
+		if (! (isset ( $this->form->kwota ) && isset ( $this->form->czas ) && isset ( $this->form->oprocentowanie ))) {
+			// sytuacja wystąpi kiedy np. kontroler zostanie wywołany bezpośrednio - nie z formularza
+			return false; //zakończ walidację z błędem
+		}
+		
+		
+		if ($this->form->kwota == "") {
+			$this->msgs->addError('Nie podano kwoty');
+		}
+		if ($this->form->czas == "") {
+			$this->msgs->addError('Nie podano czasu');
+		}
+		if ($this->form->oprocentowanie == "") {
+			$this->msgs->addError('Nie podano oprocentowania');
+		}
+		
+		
+		if (! $this->msgs->isError()) {
+			
+			
+			if (! is_numeric ( $this->form->kwota )) {
+				$this->msgs->addError('Pierwsza wartość nie jest liczbą całkowitą');
+			}
+			
+			if (! is_numeric ( $this->form->czas )) {
+				$this->msgs->addError('Druga wartość nie jest liczbą całkowitą');
+			}
+			
+			if (! is_numeric ( $this->form->oprocentowanie )) {
+				$this->msgs->addError('Druga wartość nie jest liczbą całkowitą');
+			}
+		}
+		
+		return ! $this->msgs->isError();
+	}
+	
+	/* 
+	  Pobranie wartości, walidacja, obliczenie i wyświetlenie
+	 */
+	public function process(){
+
+		$this->getparams();
+		
+		if ($this->validate()) {
+				
+			//konwersja parametrów na int
+			$this->form->kwota = intval($this->form->kwota);
+			$this->form->czas = intval($this->form->czas);
+			$this->form->oprocentowanie = intval($this->form->oprocentowanie);
+			$this->msgs->addInfo('Parametry poprawne.');
+				
+			//wykonanie operacji
+		if ($this->form->kwota < 1 ){
+		$this->msgs->addError('Nie możesz liczyć dla kwot mniejszych od 1pln !');
+				
+			} else {
+				$this->result->result = $this->form->kwota / (12*$this->form->czas)+($this->form->kwota/(12*$this->form->czas))*$this->form->oprocentowanie;
+			}		
+			$this->msgs->addInfo('Wykonano obliczenia.');
+		}
+		
+		$this->generateView();
+	}
+	
+	/**
+	 * Wygenerowanie widoku
+	 */
+	public function generateView(){
+		//nie trzeba już tworzyć Smarty i przekazywać mu konfiguracji i messages
+		// - wszystko załatwia funkcja getSmarty()
+		
+		getSmarty()->assign('page_title','Przykład 06a');
+		getSmarty()->assign('page_description','Aplikacja z jednym "punktem wejścia". Zmiana w postaci nowej struktury foderów, skryptu inicjalizacji oraz pomocniczych funkcji.');
+		getSmarty()->assign('page_header','Kontroler główny');
+					
+		getSmarty()->assign('form',$this->form);
+		getSmarty()->assign('res',$this->result);
+		
+		getSmarty()->display('CalcView.html'); // już nie podajemy pełnej ścieżki - foldery widoków są zdefiniowane przy ładowaniu Smarty
+	}
+}
